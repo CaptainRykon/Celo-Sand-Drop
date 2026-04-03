a700864cd602764c23f8e12c59fc728aad92619f
Rykon <81365827+CaptainRykon@users.noreply.github.com>
2026-04-03 13:42:37 +0530

    Update layout.tsx

--- packages/react-app/app/layout.tsx
+++ packages/react-app/app/layout.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import { AppProvider } from '@/providers/AppProvider'
 
+export const metadata = {
+    other: {
+        "talentapp:project_verification":
+            "449bc4472ca4cfb6b0fea41d9faf64bad1844b04d812b9b5e66dc47e65f403351099a9d6276099867ac61074b88b2949aafd9e5b1f558f68f4df3a5dcc202002",
+    },
+}
+
 export default function RootLayout({
     children,
 }: {

a98d2c7651a662836bfa2d006826228729940bec
Rykon <81365827+CaptainRykon@users.noreply.github.com>
2026-04-22 03:00:46 +0530

    ss

--- packages/react-app/public/game/ServiceWorker.js
+++ packages/react-app/public/game/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-MiniPay SandDrop-1.0";
 const contentToCache = [
     "Build/85b74916a9e60850f1e81997d93b62ff.loader.js",
-    "Build/aae0786fd8f24e475f83a0221a123f3d.framework.js.br",
-    "Build/6557e268b0432819472c08e6d2dc004f.data.br",
-    "Build/cb19f8d2a816eb3aeffb9905799618ee.wasm.br",
+    "Build/aae0786fd8f24e475f83a0221a123f3d.framework.js",
+    "Build/b2bea87d9a892556156eaa129579ef20.data",
+    "Build/cb19f8d2a816eb3aeffb9905799618ee.wasm",
     "TemplateData/style.css"
 
 ];
